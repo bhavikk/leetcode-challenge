6eea9e64b493a4933deb1877a76fccf9345df918
bhavikk <bhavikk.shah33@gmail.com>
2023-12-20 03:20:15 -0600

    Day9

---
+++ 75 Days Challenge/Day9/car-fleet.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/car-fleet/
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        map<int, double> m;
+        for (int i = 0; i < position.size(); i++) m[-position[i]] = (double)(target - position[i]) / speed[i];
+        int res = 0; double cur = 0;
+        for (auto it : m) if (it.second > cur) cur = it.second, res++;
+        return res;
+    }
+};
