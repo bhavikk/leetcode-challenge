55c1ea157ee80fa34643dfb2594f72375f4e24c2
bhavikk <bhavikk.shah33@gmail.com>
2024-02-20 07:53:23 -0600

    Day 20

---
+++ 75 Days Challenge/Day20/increasing-triplet-subsequence.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/increasing-triplet-subsequence/
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int num1=INT_MAX,num2=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            
+            if(nums[i]<=num1){
+                num1=nums[i];
+            }
+            else if(nums[i]<=num2){
+                    num2=nums[i];
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
